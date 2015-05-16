84b6751c9aa903d5f51f5bfa7c8f3569719b19e2
Amr <amr.e.dev@gmail.com>
2015-05-16 23:05:04 +0300

    bug for [-4 -3 -1 -2 -1 -2 -2 -4 -1 -5 -3 -4 -3 -3 -1 -4 -5 -3 -4 -1 -5 -2 -4 -2], -2

--- replace_me.m
+++ replace_me.m
@@ -1,7 +1,31 @@
 
-% 
-%
+% v is a vector, while a, b, and c are all scalars. 
+% The function replaces every element of v that is equal to a with b and c. 
+% x = replace_me([1 2 3],2,4,5);
+% makes x equal to [1 4 5 3]
+% c is omitted, it replaces occurrences of a with two copies of b.
+% b is also omitted, it replaces each a with two zeros.
 
-function replace_me
-
+function w = replace_me(v,a,b,c)
+    if (nargin < 3)
+        % b is omitted
+        a = zeros(1,2);
+        c = 0;
+    
+    elseif (nargin < 4)
+        % c is omitted
+        % replace occurances of a with 2 clones of b
+        a = [b , b];
+        c = b;
+    end
+  
+    if (v(v == a(1)))
+        fprintf('equal');
+        % Code here
+        % find equal elements
+        eqInd = find(v == a(1))
+        w = [v(1 : eqInd-1), b, c, v(eqInd+1 : end)];
+    else
+        w = v;
+    end
 end
