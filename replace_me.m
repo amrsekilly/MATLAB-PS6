3aa3efadb51bf4390bd152fff930cc882cec9752
Amr <amr.e.dev@gmail.com>
2015-05-22 04:23:37 +0300

    doc 2

---
+++ replace_me.m
@@ -0,0 +1,17 @@
+% Introduction to Programming with MATLAB - MOOC
+% Homework 6 - Problem 2
+
+% inputs: 
+% v: row vector on length n
+% a: a scalar to find and replace in the new vector
+% b: 1st new value "scalar"
+% c: 2nd new value "scalar"
+
+%% replace_me function
+% It takes v and search through it for 'a' scalar, 
+% when a is found, it replaces it with two values b&c.
+% c is omitted: c = b
+% b and c are omitted: b = 0; c = 0
+function w = replace_me(v,a,b,c)
+    
+end
